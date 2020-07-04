9cacf724333972e74b75fd785fb29d7229524dbe
ealitt <ealitt@gmail.com>
2020-07-03 21:15:20 -0400

    updates to file upload functions

--- js/index.js
+++ js/index.js
@@ -2,10 +2,24 @@ let editors = [],
 jsonData,
 reader;
 
+let srcElement;
+
 window.onload = function(){
     loadEditor();
 
     document.getElementById("json-format-file").addEventListener('change', loadJSON);
+    document.getElementById("html-format-file").addEventListener('change', loadJSON);
+    document.getElementById("generateHTML").addEventListener("click", generateHTML);
+}
+
+function generateHTML() {
+    let outputHTML;
+
+    let inputJSON = JSON.parse(editors[0].getValue());
+    // console.log(inputJSON.blocks);
+    inputJSON.blocks.forEach(block => {
+        console.log(block.data);
+    })
 }
 
 function loadEditor() {
@@ -15,21 +29,6 @@ function loadEditor() {
     editors[0].setTheme("ace/theme/monokai");
     editors[0].session.setMode("ace/mode/json");
     editors[0].id = "json-input";
-
-    let htmlFormat = document.getElementById("html-input-tab");
-
-    htmlFormat.childNodes.forEach((item, index) => {
-        if(index == 1){
-            document.getElementById(item.id + "-input").style.display = "block";
-        }
-        if(item.nodeType == 1){
-            document.getElementById(item.id).addEventListener("click", displayEditor);
-            editors[(index+1)/2] = ace.edit(item.id + "-input");
-            editors[(index+1)/2].setTheme("ace/theme/monokai");
-            editors[(index+1)/2].session.setMode("ace/mode/html");
-            editors[(index+1)/2].id = item.id + "-input";
-        }
-    });
     
     // let htmlOutput = document.getElementById("htmlOutput");
 
@@ -55,17 +54,46 @@ function displayEditor() {
 
 function loadJSON(event) {
     reader = new FileReader();
+    srcElement = event.srcElement;
     reader.onload = onReaderLoad;
     reader.readAsText(event.target.files[0]);
 }
 
 function onReaderLoad(event){
-    editors[0].setValue(event.target.result);
     jsonData = JSON.parse(event.target.result);
-    // editors[0].setValue(jsonData.blocks);
-    console.log(jsonData.toString());
+    setupEditor(srcElement, jsonData);
+    // editors[0].setValue(event.target.result);
 }
 
-window.onbeforeunload = function(event) {
-    event.returnValue = "REFRESHING WILL LOSE YOUR DATA";
-};
+function setupEditor(srcElement, data) {
+    if(srcElement.id == "json-format-file"){
+        editors[0].setValue(JSON.stringify(data, null, 1));
+    } else {
+        let inputTabs = document.getElementById("html-input-tab");
+        let inputBox = document.getElementById("html-input");
+
+        data.blocks.forEach((item, index) => {
+            inputBox.innerHTML += `<div id="${item.type}-input" class="html-format input"></div>`;
+            if(index == 0){
+                inputTabs.innerHTML += `<div id="${item.type}" class="type active">${item.type}</div>`;
+                document.getElementById(`${item.type}-input`).style.display = "block";
+            } else {
+                inputTabs.innerHTML += `<div id="${item.type}" class="type">${item.type}</div>`;
+            }
+        });
+
+        // event listener and editors instantiated later to allow DOM to load
+        data.blocks.forEach(item => {
+            document.getElementById(item.type).addEventListener("click", displayEditor);
+            editors.push(ace.edit(`${item.type}-input`));
+            editors[editors.length-1].setTheme("ace/theme/monokai");
+            editors[editors.length-1].session.setMode("ace/mode/html");
+            editors[editors.length-1].id = `${item.type}-input`;
+            editors[editors.length-1].setValue(item.data.html);
+        });
+    }
+}
+
+// window.onbeforeunload = function(event) {
+//     event.returnValue = "REFRESHING WILL LOSE YOUR DATA";
+// };
